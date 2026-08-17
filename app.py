4923f6871533144b704b21ede57fe0ea5ccd1065
Manjot Kaur <mk3325736@gmail.com>
2026-08-17 21:14:12 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello from InfraLearners CI/CD Pipeline!\n")
+
+if __name__ == "__main__":
+    HTTPServer(("0.0.0.0", 8080), Handler).serve_forever()
